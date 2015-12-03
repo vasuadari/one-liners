c1c4fc2e99f596e2eeffcc04c05d93c5caaa1cdf
Vasu Adari <vasuakeel@gmail.com>
2015-12-03 12:10:07 +0530

    Setting default value to -1

--- flatten_array.rb
+++ flatten_array.rb
@@ -1,1 +1,1 @@
-def f(a,c=-2);c==-2&&c=a.size;c-=1;a[0].is_a?(Array)?a+=f(a.shift):a<<a.shift;c<1?a:f(a,c);end
+def f(a,c=-1);c==-1&&c=a.size;c-=1;a[0].is_a?(Array)?a+=f(a.shift):a<<a.shift;c<1?a:f(a,c);end
